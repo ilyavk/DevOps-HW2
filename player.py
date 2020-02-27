cffa6f0e1720bd2b59bc21c700a09154c371f66e
ilya-unr <ikuznetsov@nevada.unr.edu>
2020-02-26 22:22:50 -0800

    Created Player class

---
+++ player.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def draw(self, deck):
+        self.hand.append(deck.draw())
+        return self
+
+    def showHand(self):
+        for card in self.hand:
+            card.show()
+
+            
