b4c93358ebe4f96a2e6e604a3dc470b83d8b4608
ilya-unr <ikuznetsov@nevada.unr.edu>
2020-02-27 22:00:11 -0800

    Blackjack game finished

--- blackjack.py
+++ blackjack.py
@@ -6,8 +6,6 @@ class Blackjack:
         self.dealer = Player("Dealer")
         self.gambler = Player("Gambler")
         self.deck = Deck()
-        self.deck.build()
-        self.deck.shuffle()
 
     def deal(self):
         self.dealer.draw(self.deck)
@@ -20,24 +18,61 @@ class Blackjack:
         for c in person.hand:
             if c.cardValue() == 11 or c.cardValue() == 12 or c.cardValue() == 13:
                 total += 10
+            elif c.cardValue() == 1:
+                total += 11
+                if total > 21:
+                    total -= 10
             else:
                 total += c.cardValue()
-            # have to do something for Ace being 1 or 11
         return total
 
     def showHand(self, person):
-        print("{} holds ".format(person.name))
+        print("-{} holds ".format(person.name))
         person.showHand()
 
     def hit(self, person):
         person.draw(self.deck)
+        print("{} drew {}".format(person.name, person.hand[len(person.hand)-1].val()))
+
             
 
 
 if __name__ == "__main__":
-    game = Blackjack()
-    game.deal()
-    game.showHand(game.dealer)
-    game.showHand(game.gambler)
-    d_total = game.total(game.dealer)
-    print(d_total)
+    won = False
+    while input("Would you like to play Blackjack? (y/n)") == "y":
+        game = Blackjack()
+        game.deal()
+        game.showHand(game.dealer)
+        game.showHand(game.gambler)
+
+        print("The dealer has a total of {}".format(game.total(game.dealer)))
+        print("The gambler has a total of {}".format(game.total(game.gambler)))
+
+        while input("Does the dealer want to hit? (y/n)") == "y":
+            game.hit(game.dealer)
+            print("The dealer has a total of {}".format(game.total(game.dealer)))
+            if game.total(game.dealer) > 21:
+                won = True
+                print("Dealer busted. Gambler wins!!")
+                break
+
+        while won == False and input("Does the gambler want to hit? (y/n)") == "y":
+            game.hit(game.gambler)
+            print("The gambler has a total of {}".format(game.total(game.gambler)))
+            if game.total(game.gambler) > 21:
+                print("Gambler busted. Dealer wins!!")
+                break
+
+        # no one busted so highest wins
+        if game.total(game.dealer) < 22 and game.total(game.gambler) < 22:
+            if game.total(game.dealer) > game.total(game.gambler):
+                print("Dealer wins!!")
+            elif game.total(game.dealer) < game.total(game.gambler):
+                print("Gambler wins!!")
+            else:
+                print("Gambler and Dealer are tied. House wins!!")
+
+        game.dealer.clearHand()
+        game.gambler.clearHand()
+
+    print("Thanks for playing Blackjack ^_^")
--- card.py
+++ card.py
@@ -4,7 +4,28 @@ class Card:
         self.value = value
 
     def show(self):
-        print("{} of {}".format(self.value, self.suit))
+        if self.value == 11:
+            print("{} of {}".format("Jack", self.suit))
+        elif self.value == 12:
+            print("{} of {}".format("Queen", self.suit))
+        elif self.value == 13:
+            print("{} of {}".format("King", self.suit))
+        elif self.value == 1:
+            print("{} of {}".format("Ace", self.suit))
+        else:
+            print("{} of {}".format(self.value, self.suit))
 
     def cardValue(self):
         return self.value
+
+    def val(self):
+        if self.value == 11:
+            return "Jack"
+        elif self.value == 12:
+            return "Queen"
+        elif self.value == 13:
+            return "King"
+        elif self.value == 1:
+            return "Ace"
+        else:
+            return self.value
